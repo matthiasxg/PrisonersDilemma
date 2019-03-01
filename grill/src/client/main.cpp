202d5d3dba2b54d1330e093086073d910a5a92b0
matthias <matthiasgrill@hotmail.com>
2019-03-01 08:27:02 +0100

    Add commandline interface to server and client

--- grill/src/client/main.cpp
+++ grill/src/client/main.cpp
@@ -1,8 +1,14 @@
 #include "client.hpp"
 #include "json.hpp"
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+#include "clipp.h"
+#pragma GCC diagnostic pop
+
 using namespace std;
 using json = nlohmann::json;
+using namespace clipp;
 
 json getJsonSettings() {
     json result;
@@ -11,9 +17,40 @@ json getJsonSettings() {
     return result;
 }
 
-//int main(int argc, char* argv[]) {
-int main() {
-    json settings = getJsonSettings();
-    Client client(ref(settings));
+int main(int argc, char* argv[]) {
+    // Logger
+    LoggingProvider logger = LoggingProvider::getInstance();
+
+    // CLI
+    short unsigned int port{0};
+    int ip{-1};
+    bool help{false};
+    bool debug{false};
+    bool strategy{false};
+
+    auto cli = (
+        option("-st", "--strategy").set(strategy).doc("your own strategy will be used"),
+        option("-s", "--server").doc("ip adress of server") & value("ip", ip),
+        option("-p", "--port").doc("port to connect to") & value("port", port),
+        option("-d", "--debug").set(debug).doc("Debug mode"),
+        option("-h", "--help").set(help).doc("help")
+    );
+
+    if (!parse(argc, argv, cli)) {
+        cout << make_man_page(cli, argv[0]);
+        return 1;
+    } else if (help) {
+        cout << make_man_page(cli, argv[0]);
+    }
+
+    // Set values to settings
+    json client_config = getJsonSettings();
+    if(ip != -1) client_config["serverIp"] = ip;
+    if(port != 0) client_config["port"] = port;
+    if(strategy) client_config["playOnCommandLine"] = false;
+    if(debug) client_config["debug"] = true;
+
+    // Start client
+    Client client(ref(client_config));
     return 0;
 }
--- grill/src/server/main.cpp
+++ grill/src/server/main.cpp
@@ -1,8 +1,15 @@
 #include "server.hpp"
 #include "json.hpp"
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+#include "clipp.h"
+#pragma GCC diagnostic pop
+
+
 using namespace std;
 using json = nlohmann::json;
+using namespace clipp;
 
 json getJsonSettings() {
     json result;
@@ -11,9 +18,36 @@ json getJsonSettings() {
     return result;
 }
 
-//int main(int argc, char* argv[]) {
-int main() {
+int main(int argc, char* argv[]) {
+    // Logger
+    LoggingProvider logger = LoggingProvider::getInstance();
+
+    // CLI
+    short unsigned int port{0};
+    bool help{false};
+    bool debug{false};
+    int rounds{-1};
+
+    auto cli = (
+        option("-p", "--port").doc("port to connect to") & value("port", port),
+        option("-r", "--rounds").doc("number of rounds you want to play") & value("rounds", rounds),
+        option("-d", "--debug").set(debug).doc("Debug mode"),
+        option("-h", "--help").set(help).doc("help")
+    );
+
+    if (!parse(argc, argv, cli)) {
+        cout << make_man_page(cli, argv[0]);
+        return 1;
+    } else if (help) {
+        cout << make_man_page(cli, argv[0]);
+    }
+
+    // Set values to settings
     json server_config = getJsonSettings();
+    if(port != 0) server_config["port"] = port;
+    if(rounds != -1) server_config["rounds"] = rounds;
+    if(debug) server_config["debug"] = true;
+
     Server server(ref(server_config));
     return 0;
 }
